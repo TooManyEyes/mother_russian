32885681f9654fbebd82417f1e2c0de6b8cdfbb0
Nikita <71151110+TooManyEyes@users.noreply.github.com>
2020-11-22 20:45:45 +0300

    Создана заготовка для будущих функций

--- start_page_example/game_map/map_script.js
+++ start_page_example/game_map/map_script.js
@@ -1,53 +1,98 @@
-var marker;
-var round = 1;
+let markerA; // Маркер панорамы
+let markerB; //Маркер игрока
+let latLngA;
+let latLngB;
 let map;
+let round = 1;
+let locationNumber = 0 // Отслеживает какую локацию нужно вывести сейчас
+let line;
+let distance;
+let score;
+const confirmButton = document.querySelector('.confirmButton');
+const continueButton = document.querySelector('.continueButton');
+const fenway = { lat: 42.345573, lng: -71.098326 };
+let moscow = {lat: 55.7538594, lng: 37.6206391};
 
-function initGameMap(listener) {
-    moscow = { lat: 55.7538594, lng: 37.6206391 }
+
+function initGameProcess(gameInfo) {
+    // let gameMode = gameInfo.get('gameMode');
+    // let locations = gameInfo.get('locations');
+    // let roundsCount = gameInfo.get('gameCount');
+    // let scoreMultiplayer = gameInfo.get('scoreMultiplicator');
+    // initGameMap(locations[locationNumber]);
+    
+    latLngA = {lat: 55.7538594, lng: 37.6206391} // locations[0]
+    initGameMap(latLngA)
+    
+    //Инициализация кнопок -------------------------------------------
+    let confirmButton = document.getElementById('confirmButton');
+    let continueButton = document.getElementById('continueButton');
+    
+    confirmButton.addEventListener('click', function(){ // Подтвердить
+                                   confirmButton.disabled = true;
+                                   continueButton.disabled = false;
+                                   updateScore();
+                                   google.maps.event.clearListeners(map, 'click');
+                                   });
+                                   
+    continueButton.addEventListener('click', function(){ // Продолжить
+                                   confirmButton.disabled = false;
+                                   continueButton.disabled = true;
+                                   updateRound();
+                                   updateMap();
+                                   });
+}
+
+// game_mode - это словарь, который передает следующие сведения:
+// 1 - название режима игры (gameMode),  2 - список локаций (locations),
+// 3 - количество раундов (roundsCount), 4 - модификатор режима игры (scoreMultiplicator)
+
+
+function initGameMap(latLngA) {
     map = new google.maps.Map(document.getElementById("game_map"), {
-    zoom: 12,
-    center: moscow,
-    mapTypeId: "roadmap",
-    disableDefaultUI: true,
+        zoom: 12,
+        center: moscow,
+        mapTypeId: "roadmap",
+        disableDefaultUI: true,
+        clickableIcons: false,
     });
 
-  google.maps.event.addListener(map, "click", function (event) {
-      latLng = event.latLng;
+    google.maps.event.addListener(map, "click", function (event) {
+        latLngB = event.latLng;
+        if (markerB && markerB.setMap) {
+            markerB.setMap(null);
+        }
+        markerB = new google.maps.Marker({
+            position: latLngB,
+            map: map,
+            Clickable: false,
+        });
+    });
 
-      if (marker && marker.setMap) {
-          marker.setMap(null);
-      }
-      marker = new google.maps.Marker({
-          position: latLng,
-          map: map,
-          Clickable: false,
-      });
-      getCoordinates(latLng)
-  });
+    panorama = new google.maps.StreetViewPanorama(
+        document.getElementById("game_panorama"), {
+            position: latLngA,
+            pov: {heading: 34, pitch: 10,},
+            addressControl: false,
+            enableCloseButton: false,
+        });
 
-    
-  panorama = new google.maps.StreetViewPanorama(
-      document.getElementById("game_panorama"),{
-      position: moscow,
-      pov: {heading: 34, pitch: 10,},
-      addressControl: false,
-      enableCloseButton: false,
-      });
-    
-  map.setStreetView(panorama);     
+    map.setStreetView(panorama);
+}
+
+
+function updateMap() {
+    //TODO
+}
+
+let updateRound = function () {
+    //TODO
 };
 
+let showDistance = function () { 
+    //TODO
+}
 
-function getCoordinates(location){
-    const coordinates = location
-    alert(location)
-};
-
-var updateRound = function() {
-    var round = document.getElementById('round');
-    var count = Number(round.innerHTML);
-    round.innerHTML = count += 1;
-};
-
-
-//setInterval(updateRound, 20)
+let updateScore = function () {
+    //TODO
+}
